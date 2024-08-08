ce5f92dc037b836a0c57cfcb589126b2f26da1e5
Mia Sormaz <miasormaz2006@gmail.com>
2024-08-08 11:52:34 +1000

    moved in 2 of the condition functions

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-import __init__
+import init
 
+def conditions():
+    if init.blindness == 'true':
+        init.perc = 0
+        init.passivePerc = 0
+    elif init.blindness == 'false':
+        if init.percProf == 'true':
+            init.perc = init.wisMod+init.profBonus
+            init.passivePerc = 10+init.wisMod+init.profBonus
+        elif init.percProf=='false':
+            init.perc=init.wisMod
+            init.passivePerc=10+init.wisMod
+    if init.charmed == 'true':
+        init.pers=init.pers+init.profBonus
+    elif init.charmed == 'false':
+        if init.persProf == 'true':
+            init.pers=init.chaMod+init.profBonus
+        elif init.persProf == 'false':
+            init.pers=init.chaMod
